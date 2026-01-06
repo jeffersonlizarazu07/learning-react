d9a5549814a9948a699a4202bd24a691eb3c2fd3
Jefferson Lizarazu <Jeffersonlizarazu@hotmail.com>
2026-01-06 17:38:39 -0500

    heroes.data.ts created

--- src/bases/07-array-destructuring.ts
+++ src/bases/07-array-destructuring.ts
@@ -1,11 +1,26 @@
-const names: string[] = ["Jefferson", "Johan", "Luz", "Yamile", "Teresa"];
-const [, , , , name] = names;
+// const names: string[] = ["Jefferson", "Johan", "Luz", "Yamile", "Teresa"];
+// const [, , , , name] = names;
 
-console.log({ name });
+// console.log({ name });
 
-const returnArrayFn = () => {
-  return ["ABC", 123] as const;
+// const returnArrayFn = () => {
+//   return ["ABC", 123] as const;
+// };
+
+// const [letters, numbers] = returnArrayFn();
+// console.log(letters, numbers);
+
+// Práctica
+
+const useState = (value: string) => {
+  return [
+    value,
+    (name: string) => {
+      return console.log(name);
+    },
+  ] as const;
 };
 
-const [letters, numbers] = returnArrayFn();
-console.log(letters, numbers);
+const [name, setName] = useState("Goku");
+console.log(name);
+setName("Vegeta");
